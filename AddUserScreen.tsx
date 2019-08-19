c41e1deaf225186bfb9af566e5a9b1374815557b
marcosppm <marcos_ppm@live.com>
2019-08-19 11:26:19 -0300

    Changed event for evaluating the form fields (now only at the end of filling)

--- AddUserScreen.tsx
+++ AddUserScreen.tsx
@@ -42,20 +42,29 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 });
 AppRegistry.registerComponent('AddUserScreen', () => ApolloApp);
 
+const EMPTY_ROLE: string = "empty";
 const USER: string = "user";
 const ADMIN: string = "admin";
 
 enum UserRoleType {
-  admin,
-  user
+  admin="admin",
+  user="user"
+}
+
+enum DateMessage {
+  INVALID_DATE,
+  DATE_BEFORE_TODAY,
+  CORRECT_DATE
 }
 
 interface AddUserScreenState {
   name: string,
   cpf: string,
   birthDate: string,
+  birthDateFormatted: string,
   email: string,
   password: string,
+  selectedRole: string,
   userRole: UserRoleType,
   errorMessage: string
 }
@@ -72,7 +81,8 @@ interface UserInput {
 export default class AddUserScreen extends Component<AddUserScreenProps, AddUserScreenState> {
   constructor(props: AddUserScreenProps) {
     super(props);
-    this.state = { name: "", cpf: "", birthDate: "", email: "", password: "", userRole: null, errorMessage: "" };
+    this.state = { name: "", cpf: "", birthDateFormatted: "", birthDate: "", email: "", password: "", selectedRole: "",
+                   userRole: null, errorMessage: "" };
   }
 
   static navigationOptions = {
@@ -94,7 +104,7 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
           mutation={this.mutationCreateUser}
           variables={{ input: {
             name: this.state.name, email: this.state.email, cpf: this.state.cpf,
-            birthDate: this.state.birthDate, password: this.state.password,
+            birthDate: this.state.birthDateFormatted, password: this.state.password,
             role: this.state.userRole } }}
         >
           {(mutateFunction: MutationFunction<{UserCreate: {id: number}}, {input: UserInput}>, { loading }) => {
@@ -104,14 +114,14 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
                 <TextInput
                   style={styles.textInput}
                   autoCapitalize='words'
-                  onEndEditing={(event) => this.verifyName(event.nativeEvent.text)}
+                  onChangeText={(text) => this.setState({ name: text })}
                 />
 
                 <Text style={styles.fieldTitle}>CPF:</Text>
                 <TextInput
                   style={styles.textInputWithTip}
                   autoCapitalize='none'
-                  onEndEditing={(event) => this.verifyCPF(event.nativeEvent.text)}
+                  onChangeText={(text) => this.setState({ cpf: text })}
                 />
                 <Text style={styles.textTip}>(Utilize pontos e traços)</Text>
 
@@ -119,7 +129,7 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
                 <TextInput
                   style={styles.textInputWithTip}
                   autoCapitalize='none'
-                  onEndEditing={(event) => this.verifyBirthdate(event.nativeEvent.text)}
+                  onChangeText={(text) => this.setState({ birthDate: text })}
                 />
                 <Text style={styles.textTip}>(Formato: dd/mm/yyyy)</Text>
 
@@ -127,7 +137,7 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
                 <TextInput
                   style={styles.textInput}
                   autoCapitalize='none'
-                  onEndEditing={(event) => this.verifyEmail(event.nativeEvent.text)}
+                  onChangeText={(text) => this.setState({ email: text })}
                 />
 
                 <Text style={styles.fieldTitle}>Senha:</Text>
@@ -137,15 +147,15 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
                   autoCapitalize='none'
                   underlineColorAndroid='transparent'
                   secureTextEntry={true}
-                  onEndEditing={(event) => this.verifyPassword(event.nativeEvent.text)}
+                  onChangeText={(text) => this.setState({ password: text })}
                 />
 
                 <Text style={styles.fieldTitle}>Função:</Text>
                 <Picker
-                  selectedValue={this.state.userRole}
+                  selectedValue={this.state.selectedRole}
                   onValueChange={(itemValue) => {
-                    this.verifyRole(itemValue);
-                    this.setState({ userRole: itemValue });
+                    this.setRole(itemValue);
+                    this.setState({ selectedRole: itemValue });
                   }}>
                   <Picker.Item label="" value="empty" />
                   <Picker.Item label="System Administrator" value="admin" />
@@ -156,7 +166,7 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
                   title="Cadastrar"
                   color="#9400D3"
                   onPress={() => this.handleButtonPress(mutateFunction)}
-                  disabled={this.disabledButton() || loading}
+                  disabled={loading}
                 />
 
                 <View style={{ alignItems: 'center' }}>
@@ -172,106 +182,128 @@ export default class AddUserScreen extends Component<AddUserScreenProps, AddUser
   }
 
   private handleButtonPress = async (mutateFunction): Promise<void> => {
-    try {
-      const result = await mutateFunction({ variables: {
-        name: this.state.name, email: this.state.email, cpf: this.state.cpf,
-        birthDate: this.state.birthDate, password: this.state.password, role: this.state.userRole
-      } });
+    if (this.correctlyFilled()) {
+      try {
+        const result = await mutateFunction({ variables: {
+          name: this.state.name, email: this.state.email, cpf: this.state.cpf,
+          birthDate: this.state.birthDateFormatted, password: this.state.password, role: this.state.userRole
+        } });
 
-      if (result.error) {
-        const message: string = result.error.graphQLErrors[0].message;
+        if (result.error) {
+          const message: string = result.error.graphQLErrors[0].message;
+          this.setState({ errorMessage: message });
+
+        } else if (result.data) {
+          let id: number = result.data.UserCreate.id;
+          this.setState({ errorMessage: "" });
+          alert("Usuário " + id + " criado com sucesso.");
+          this.props.navigation.navigate('UserList');
+        }
+      } catch (error) {
+        const message: string = error.message;
+        console.log(JSON.stringify(error));
         this.setState({ errorMessage: message });
-
-      } else if (result.data) {
-        let id: number = result.data.UserCreate.id;
-        this.setState({ errorMessage: "" });
-        alert("Usuário " + id + " criado com sucesso.");
-        this.props.navigation.navigate('UserList');
       }
-    } catch (error) {
-      const message: string = error.message;
-      this.setState({ errorMessage: message });
     }
   }
 
-  private disabledButton = (): boolean => {
-    return (!this.state.name || !this.state.cpf || !this.state.birthDate ||
-            !this.state.email || !this.state.password || !this.state.userRole);
+  private correctlyFilled = (): boolean => {
+    if (!this.state.name || !this.validName()) {
+      this.setState({ errorMessage: "Digite um nome válido." });
+      return false;
+    } else if (!this.state.cpf || !this.validCPF()) {
+      this.setState({ errorMessage: "Digite um CPF válido." });
+      return false;
+    } else if (!this.state.birthDate || !this.validDateFormat()) {
+      this.setState({ errorMessage: "Utilize o formato dd/mm/yyyy." });
+      return false;
+    } else {
+      const dateMessage: DateMessage = this.validDate();
+      switch (dateMessage) {
+        case DateMessage.INVALID_DATE:
+          this.setState({ errorMessage: "Data inválida." });
+          return false;
+        case DateMessage.DATE_BEFORE_TODAY:
+          this.setState({ errorMessage: "Data deve ser anterior a hoje." });
+          return false;
+        case DateMessage.CORRECT_DATE:
+          this.setState({ errorMessage: "" });
+      }
+      if (!this.state.email || !this.validEmail()) {
+        this.setState({ errorMessage: "Digite um e-mail válido." });
+        return false;
+      } else if (this.state.password.length < 7) {
+        this.setState({ errorMessage: "A senha deve ter pelo menos 7 caracteres." });
+        return false;
+      } else if (!this.state.password || !this.validPasswordFormat()) {
+        this.setState({ errorMessage: "A senha deve conter pelo menos um caracter e um dígito." });
+        return false;
+      } else if (!this.roleSelected()) {
+        this.setState({ errorMessage: "Selecione uma função para o usuário." });
+        return false;
+      }
+    }
+    return true;
   }
 
-  private verifyName(name: string): void {
+  private validName = (): boolean => {
     const regexName: RegExp = /^[a-zA-Z ]*$/gm;
-    if (regexName.test(name)) {
-      this.setState({ name: name, errorMessage: "" });
-    } else {
-      this.setState({ name: "", errorMessage: "Digite um nome válido." });
-    }
+    return regexName.test(this.state.name);
   }
 
-  private verifyCPF(cpf: string): void {
+  private validCPF = (): boolean => {
     const regexCPF: RegExp = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/gm;
-    if (regexCPF.test(cpf)) {
-      this.setState({ cpf: cpf, errorMessage: "" });
-    } else {
-      this.setState({ cpf: "", errorMessage: "Digite um CPF válido." });
-    }
+    return regexCPF.test(this.state.cpf);
   }
 
-  private verifyBirthdate(inputDate: string): void {
+  private validDateFormat = (): boolean => {
     const regexBirthdate: RegExp = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/gm;
+    return regexBirthdate.test(this.state.birthDate);
+  }
 
-    if (!regexBirthdate.test(inputDate)) {
-      this.setState({ errorMessage: "Utilize o formato dd/mm/yyyy" });
+  private validDate = (): DateMessage => {
+    const tokens: string[] = this.state.birthDate.split('/');
+    const day: number = parseInt(tokens[0]);
+    const month: number = parseInt(tokens[1]);
+    const year: number = parseInt(tokens[2]);
+    const birthDateFormatted: string = year + "-" + month + "-" + day;
+    console.log(birthDateFormatted);
+
+    const birthDate: Date = new Date(birthDateFormatted);
+    const dateNow: Date = new Date();
+
+    if (!birthDate.valueOf()) {
+      return DateMessage.INVALID_DATE;
+    } else if (dateNow.getTime() <= birthDate.getTime()) {
+      return DateMessage.DATE_BEFORE_TODAY;
     } else {
-      const tokens: string[] = inputDate.split('/');
-      const day: number = parseInt(tokens[0]);
-      const month: number = parseInt(tokens[1]);
-      const year: number = parseInt(tokens[2]);
-      const birthdateText: string = year + "-" + month + "-" + day;
-
-      const birthDate: Date = new Date(birthdateText);
-      const dateNow: Date = new Date();
-
-      if (!birthDate.valueOf()) {
-        this.setState({ birthDate: "", errorMessage: "Data inválida" });
-      } else if (dateNow.getTime() <= birthDate.getTime()) {
-        this.setState({ birthDate: "", errorMessage: "Data de nascimento deve \nser anterior a hoje." });
-      } else {
-        this.setState({ birthDate: birthdateText, errorMessage: "" });
-      }
+      this.setState({ birthDateFormatted: birthDateFormatted });
+      return DateMessage.CORRECT_DATE;
     }
   }
 
-  private verifyEmail(email: string): void {
+  private validEmail = (): boolean => {
     const regexEmail: RegExp = /([A-Za-z])+@([A-Za-z])+.com/gm;
-    if (regexEmail.test(email)) {
-      this.setState({ email: email, errorMessage: "" });
-    } else {
-      this.setState({ email: "", errorMessage: "Digite um e-mail válido." });
-    }
+    return regexEmail.test(this.state.email);
   }
 
-  private verifyPassword(password: string): void {
+  private validPasswordFormat(): boolean {
     const regexOneCharAndOneDigit: RegExp = /(?=.*?[0-9])(?=.*?[A-Za-z]).+/gm;
-    if (!password) {
-      this.setState({ password: "", errorMessage: "Senha obrigatória." });
-    } else if (password.length < 7) {
-      this.setState({ password: "", errorMessage: "A senha deve ter pelo menos 7 caracteres." });
-    } else if (!regexOneCharAndOneDigit.test(password)) {
-      this.setState({ password: "", errorMessage: "A senha deve conter pelo menos um caracter e um dígito" });
+    return regexOneCharAndOneDigit.test(this.state.password);
+  }
+
+  private setRole(selectedItem: string): void {
+    if (selectedItem == ADMIN) {
+      this.setState({ userRole: UserRoleType.admin });
+    } else if (selectedItem == USER) {
+      this.setState({ userRole: UserRoleType.user });
     } else {
-      this.setState({ password: password, errorMessage: "" });
+      this.setState({ userRole: null });
     }
   }
 
-  private verifyRole(role: string): void {
-    if (role == ADMIN) {
-      this.setState({ userRole: UserRoleType.admin, errorMessage: "" });
-    } else if (role == USER) {
-      this.setState({ userRole: UserRoleType.user, errorMessage: "" });
-    } else {
-      this.setState({ userRole: null, errorMessage: "Escolha a função do usuário." });
-    }
+  private roleSelected(): boolean {
+    return (this.state.userRole != null);
   }
 }
 
